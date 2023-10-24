344183a2f5189b48bd0ce72ce10d471f102626cd
Laila Case <lai_case@hotmail.com>
2023-10-24 14:59:15 +0100

    Pass tests for Media and Book classes

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,22 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static highestRating(books) {
+    return books.reduce((a, b) => (a.rating > b.rating ? a : b));
+  }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,11 @@
 // create your Media class:
-
+class Media {
+  constructor(title, year, genre, totalMediaCount) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+    this.totalMediaCount = totalMediaCount;
+  }
+}
 // uncomment below to export it:
 module.exports = Media;
